f9439865d5a91a5438f084ff181d71ea88741f14
Charana <charana@Charanas-MacBook-Pro.local>
2019-01-06 03:48:02 +0000

    fixed database api?

--- models/model.js
+++ models/model.js
@@ -17,143 +17,180 @@ var Model = function(){
 
 }
 
-CONFIG = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+Model.CONFIG = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
 
-	// Returns a promise that returns all events
+// Returns a promise that returns all events
+// output = [event::<string>]
 Model.getEvents = function(){
-		return ddb.scan({
-			TableName: CONFIG.ddb_event_table,
-			ProjectionExpression: "eventt"
-		}).promise().then(data => {
-			return data.Items.map(event_record => {
-				return event_record.eventt.S
-			})
-		})
-	}
+    return ddb.scan({
+        TableName: Model.CONFIG.ddb_event_table,
+        ProjectionExpression: "eventt"
+    }).promise().then(data => {
+        return data.Items.map(event_record => {
+            return event_record.eventt.S
+        })
+    })
+}
 
-	// Returns promise that returns an image and its metadata
+// Returns promise that returns an image and its metadata
+// output = image = { data::<byte data>, meta_data: { filename::<string>, content_type::<string>, eventt::<string> }
 Model.getImageByKey = function(image_meta){
 
-		s3_uri = `${image_meta.eventt}/${image_meta.filename}`
+    s3_uri = `${image_meta.eventt}/${image_meta.filename}`
 
-		fetchImageP = s3.getObject({
-			Bucket: CONFIG.s3_image_bucket,
-			Key: s3_uri
-		}).promise().then(data => {
-			return data.Body
-		})
+    fetchImageP = s3.getObject({
+        Bucket: Model.CONFIG.s3_image_bucket,
+        Key: s3_uri
+    }).promise().then(data => {
+        return data.Body
+    })
 
-		fetchImageMetadataP = ddb.query({
-			TableName: CONFIG.ddb_image_table,
-			KeyConditionExpression: 'eventt = :eventt AND s3_uri = :s3_uri',
-			ProjectionExpression: 's3_uri, filename, content_type, eventt',
-			ExpressionAttributeValues: {
-				':s3_uri': { 'S': s3_uri },
-				':eventt': { 'S': image_meta.eventt }
-			}
-		}).promise().then(image_records => {
-			return image_records.Items.map(image_record => {
-				return {
-					s3_uri: image_record.s3_uri.S,
-					filename: image_record.filename.S,
-					content_type: image_record.content_type.S,
-					event: image_record.content_type.S
-				}
-			})
-		})
+    fetchImageMetadataP = ddb.query({
+        TableName: Model.CONFIG.ddb_image_table,
+        KeyConditionExpression: 'eventt = :eventt AND s3_uri = :s3_uri',
+        ProjectionExpression: 's3_uri, filename, content_type, eventt',
+        ExpressionAttributeValues: {
+            ':s3_uri': { 'S': s3_uri },
+            ':eventt': { 'S': image_meta.eventt }
+        }
+    }).promise().then(image_records => {
+        return image_records.Items.map(image_record => {
+            return {
+                s3_uri: image_record.s3_uri.S,
+                filename: image_record.filename.S,
+                content_type: image_record.content_type.S,
+                event: image_record.eventt.S
+            }
+        })
+    })
 
-		return Promise.all([fetchImageP, fetchImageMetadataP])
-		.then(([image_data, image_metadata]) => {
-			return {
-				data: image_data,
-				metadata: image_metadata
-			}
-		})
-	}
+    return Promise.all([fetchImageP, fetchImageMetadataP])
+    .then(([image_data, image_metadata]) => {
+        return {
+            data: image_data,
+            metadata: image_metadata
+        }
+    })
+}
 
-	// Returns a promise that returns all images (and assosiated metadata) for a given event
-Model.getImageByEvent = function(eventt){
+// Returns a promise that returns all images (and assosiated metadata) for a given event
+// output = [image]
+Model.getImagesByEvent = function(eventt){
 
-		return ddb.query({
-			TableName: CONFIG.ddb_image_table,
-			KeyConditionExpression: 'eventt = :ev',
-			ProjectionExpression: "eventt, s3_uri",
-			ExpressionAttributeValues: {
-				':evt': { 'S': eventt }
-			}
-		}).promise().then(image_records => {
-			image_records = image_records.Items.map(image_record => {
-				return {
-					eventt: image_record.eventt.S,
-					s3_uri: image_record.s3_uri.S
-				}
-			})
-			getImagesP = image_records.map(image_record => getImageByKey(image_record.s3_uri))
-			return Promise.all(getImagesP)
-		})
-	}
+    return ddb.query({
+        TableName: Model.CONFIG.ddb_image_table,
+        KeyConditionExpression: 'eventt = :eventt',
+        ProjectionExpression: "eventt, filename",
+        ExpressionAttributeValues: {
+            ':eventt': { 'S': eventt }
+        }
+    }).promise().then(image_records => {
+        image_records = image_records.Items.map(image_record => {
+            return {
+                eventt: image_record.eventt.S,
+                filename: image_record.filename.S
+            }
+        })
+        getImagesP = image_records.map(image_record => Model.getImageByKey(image_record))
+        return Promise.all(getImagesP)
+    })
+}
 
-	// Returns a promise that uploads an image to s3 and create a DynamoDB metadata record
+// Returns a promise that uploads an image to s3 and create a DynamoDB metadata record
+// output = undefined
 Model.uploadImage = function(image_data, image_metadata){
 
-		s3_uri = `${image_metadata.eventt}/${image_metadata.filename}`
+    s3_uri = `${image_metadata.eventt}/${image_metadata.filename}`
 
-		uploadS3 = s3.putObject({
-			Body: image_data,
-			Bucket: CONFIG.s3_image_bucket,
-			Key: s3_uri
-		}).promise()
+    uploadS3 = s3.putObject({
+        Body: image_data,
+        Bucket: Model.CONFIG.s3_image_bucket,
+        Key: s3_uri
+    }).promise()
 
-		createDDBEntry = ddb.putItem({
-			TableName: CONFIG.ddb_image_table,
-			ReturnConsumedCapacity: 'TOTAL',
-			Item: {
-				s3_uri: { S: s3_uri },
-				eventt: { S: image_metadata.eventt },
-				filename: { S: image_metadata.filename },
-				content_type: { S: image_metadata.content_type },
-			}
-		}).promise()
+    createDDBEntry = ddb.putItem({
+        TableName: Model.CONFIG.ddb_image_table,
+        ReturnConsumedCapacity: 'TOTAL',
+        Item: {
+            s3_uri: { S: s3_uri },
+            eventt: { S: image_metadata.eventt },
+            filename: { S: image_metadata.filename },
+            content_type: { S: image_metadata.content_type },
+        }
+    }).promise()
 
-		return uploadS3
-			.then(() => createDDBEntry)
-	}
+    return uploadS3
+        .then(() => createDDBEntry)
+}
 
-	// Returns a promise that updates an images DynamoDB metadata and (if necessary) moves the image in S3
+// Returns a promise that delete an image in s3 and deletes its assosiated a DynamoDB metadata record
+// output = undefined
+Model.deleteImage = function(image_metadata){
+
+    s3_uri = `${image_metadata.eventt}/${image_metadata.filename}`
+
+    deleteDDBEntry = ddb.deleteItem({
+        TableName: Model.CONFIG.ddb_image_table,
+        Key:{
+            eventt: { "S": image_metadata.eventt },
+            s3_uri: { "S": s3_uri }
+        }
+    }).promise()
+
+    deleteObject = s3.deleteObject({
+        Bucket: Model.CONFIG.s3_image_bucket,
+        Key: s3_uri
+    }).promise()
+
+    return deleteOldObject
+        .then(() => deleteDDBEntry)
+}
+
+// Returns a promise that updates an images DynamoDB metadata and (if necessary) moves the image in S3
+// output = undefined
 Model.updateImageMetaData = function(old_image_metadata, new_image_metadata){
-		old_s3_uri = `${old_old_image_metadata.eventt}/${old_old_image_metadata.filename}.${old_old_image_metadata.content_type}`
-		new_s3_uri = `${new_image_metadata.eventt}/${new_image_metadata.filename}.${new_image_metadata.content_type}`
+    old_s3_uri = `${old_image_metadata.eventt}/${old_image_metadata.filename}`
+    new_s3_uri = `${new_image_metadata.eventt}/${new_image_metadata.filename}`
 
-		updateDDBRecord = ddb.updateItem({
-			Key: {
-				"s3_uri": { S: old_s3_uri },
-				"eventt": { S: old_image_metadata.eventt }
-			},
-			UpdateExpression: `SET s3_uri = ${new_s3_uri}, eventt = ${new_eventt}, filename: ${new_image_metadata.filename}, content_type: ${new_image_metadata.content_type}`
-		}).promise()
+    return s3.copyObject({
+        Bucket: Model.CONFIG.s3_image_bucket,
+        CopySource: Model.CONFIG.s3_image_bucket + '/' + old_s3_uri,
+        Key: new_s3_uri
+    }).promise()
+    .then(() => {
+    	return s3.waitFor('objectExists', {
+    		Bucket: Model.CONFIG.s3_image_bucket,
+    		Key: new_s3_uri
+    	}).promise()
+    })
+    .then(() => {
+        return s3.deleteObject({
+            Bucket: Model.CONFIG.s3_image_bucket,
+            Key: old_s3_uri
+        }).promise()
+    })
+    .then(() => {
+        return ddb.deleteItem({
+            TableName: Model.CONFIG.ddb_image_table,
+            Key:{
+                eventt: { "S": old_image_metadata.eventt },
+                s3_uri: { "S": old_s3_uri }
+            }
+        }).promise()
+    })
+    .then(() => {
+        createDDBEntry = ddb.putItem({
+            TableName: Model.CONFIG.ddb_image_table,
+            ReturnConsumedCapacity: 'TOTAL',
+            Item: {
+                s3_uri: { "S": new_s3_uri },
+                eventt: { "S": new_image_metadata.eventt },
+                filename: { "S": new_image_metadata.filename },
+                content_type: { "S": new_image_metadata.content_type }
+            }
+        }).promise()
+    })
 
-		copyObject = ddb.copyObject({
-			Bucket: CONFIG.s3_image_bucket,
-			CopySource: old_s3_uri,
-			Key: new_s3_uri
-		}).promise()
-
-		deleteOldObject = ddb.deleteObject({
-			Bucket: CONFIG.s3_image_bucket,
-			Key: old_s3_uri
-		}).promise()
-
-		moveObject = copyObject.then(() => deleteOldObject)
-		if (old_s3_uri !== new_s3_uri) return updateDDBRecord.then(() => moveObject)
-		else return updateDDBRecord
-	}
-
-
-
-
-
-
-
-
+}
 
 module.exports = Model;
--- models/tests/model_tests.js
+++ models/tests/model_tests.js
@@ -1,21 +1,85 @@
-var model = require("../model.js")
+const model = require("../model.js")
+const fs = require('fs');
 
 // Setup and Sync Dev S3 Bucket and DynamoDB Tables
-require("../dev/setup.js")
+// require("../dev/setup.js")
 
 // Modify CONFIG to point to Dev resources
 model.CONFIG = {
-	"s3_image_bucket": "photofolio-dev-s3-bucket",
+	"s3_image_bucket": "photofolio-dev-s3-bucket-2",
 	"ddb_image_table": "dev-images",
 	"ddb_event_table": "dev-events"
 }
+var eventt = "a_colourful_world"
+var filename = "b1-f1.jpg"
+var file = fs.readFileSync("./file.jpg")
 
-// TODO
-	// getEvents
-	// getImageByKey
-	// getImageByEvent
-	// uploadImage
-	// updateImageMetaData
+// getEvents
+// model.getEvents()
+// .then(events => console.log("events: " + events))
+// .catch(err => console.log("err: " + err))
+
+// getImageByKey
+// model.getImageByKey({"eventt": eventt, "filename": filename})
+// .then(image => {
+//     console.log(JSON.stringify(image.metadata))
+//     fs.writeFile("./file.jpg", Buffer.from(image.data), (err) => console.log(err))
+// })
+
+// getImagesByEvent
+// model.getImagesByEvent(eventt)
+// .then(images => {
+//     images.forEach((image, i) => {
+//         console.log(JSON.stringify(image.metadata))
+//         fs.writeFile("./file" + i + ".jpg", Buffer.from(image.data), (err) => console.log(err))
+//     })
+// })
+// .catch(err => console.log(err))
+
+// uploadImage
+
+// deleteImage
+// model.deleteImage({eventt: eventt, filename: "newfile.jpg"})
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// updateImageMetaData
+// model.uploadImage(file, {eventt: eventt, filename: "newfile.jpg", content_type: ".jpg"})
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+// .then(() => model.updateImageMetaData({eventt: eventt, filename: "newfile.jpg", content_type: ".jpg"}, {eventt: eventt, filename: "newfile1.jpg", content_type: ".jpg"}))
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
